23b93def6539ff97195b046b7b54f3b5c8949a23
kawfeee <kaifalikhan347@gmail.com>
2025-03-11 21:34:43 +0530

    sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
 
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>Home page content</h1>
