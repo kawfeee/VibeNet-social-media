bb3d0bf2a51b9871c90c27a41e1aba0a46edb487
kawfeee <kaifalikhan347@gmail.com>
2025-04-02 23:51:13 +0530

    toggleLike

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+"use client";
 
-function PostCard() {
+// import {Post} from "@prisma/client"
+import{ getPosts, toggleLike } from "@/actions/post.action";
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+
+
+function PostCard({post, dbUserId} : {post:Post; dbUserId:string | null }) {
+  const { user } = useUser();
+  const [newComment, setNewComment] = useState("");
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [ hasLiked, setHasLiked] = useState(false);
+  const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes)
+
+  const handleLike = async () => {
+    if (isLiking) return;
+    try {
+      setIsLiking(true);
+      setHasLiked((prev) => !prev);
+      setOptimisticLikes((prev) => prev + (hasLiked ? -1 : 1));
+      await toggleLike(post.id);
+    } catch (error) {
+      setOptimisticLikes(post._count.likes);
+      setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+    } finally {
+      setIsLiking(false);
+    }
+  };
+
   return (
     <div>PostCard</div>
   )
